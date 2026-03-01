00b37f7547e0cab2c55402744ab48f38798a2702
Kiran Richard Johny D <kiranrichardjohny@gmail.com>
2026-03-01 17:26:50 +0530

    Eureka server configuration and setup

---
+++ src/test/java/com/online/bus/ticket/reservation/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.online.bus.ticket.reservation.eureka;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
